7a491c9bb33178618840bc1e0819c02eaa641a46
Fish <fishagbalo@gmail.com>
2025-03-08 12:58:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg text-gray-600">
+        This is a sample paragraph to test Tailwind CSS.
+      </p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Click me
+      </button>
+    </>
+  );
+}
+
+export default App;
